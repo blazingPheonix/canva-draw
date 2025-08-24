4bca80a1f831d8d3427b0775760ecc4ec77e3e85
rshandilya-blaze <rishabh.shandilya22@gmail.com>
2025-08-25 01:00:30 +0530

    added jwt authentication in socket server for authentication

--- apps/socket-server/src/index.ts
+++ apps/socket-server/src/index.ts
@@ -1,23 +1,43 @@
 import express from 'express';
 import { createServer } from 'node:http';
-
-
+import jwt from 'jsonwebtoken'; 
 import { Server } from 'socket.io';
+import bcrypt from 'bcrypt';
 
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
 
 const port = 8080; 
+const JWT_SECRET = "rishabh";
 
+console.log('socket server started');
 app.get('/', (req, res) => {
   res.send('socket server running')
 });
 
 io.on('connection', (socket) => {
+  console.log('server connected');
+  console.log('socket is ',socket);
   console.log('a user connected');
+
+  const token = socket.handshake.query?.token;
+  if(!token){
+    socket.disconnect(true);
+    return;
+  }
+  const decode = jwt.verify(token,JWT_SECRET) || ""; 
+  if(!decode || decode === "" || !decode.userId) {
+    socket.disconnect(true); 
+    return ;
+  }
+
+  socket.on('message',()=>{
+    io.emit('message',"hello all");
+  })
+
 });
 
 server.listen(port, () => {
-  console.log('server running at http://localhost:3000');
+  console.log('server running at http://localhost:8080');
 });
